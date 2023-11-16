2b5d6d454447b7bc3363ca797ed85cbeb2e94baa
Felipe-gsilva <felipe.gsilva@protonmail.com>
2023-11-16 11:09:19 -0300

    Changes for later

--- Studying/Dynamic Structures/listaCircular.c
+++ Studying/Dynamic Structures/listaCircular.c
@@ -9,93 +9,31 @@ typedef struct filas
 
 } Node;
 
-void insertList(Node **head, int input)
+void insert(Node **head, int value)
 {
-  Node *temp = malloc(sizeof(Node));
-  temp->valor = input;
-  temp->next = NULL;
-  if (!(*head))
+  Node *new = malloc(sizeof(Node));
+  new->valor = value;
+  new->next = NULL;
+
+  Node *cur = *head, *prev = *head;
+  if(!(*head))
   {
-    temp->next = *head;
-    *head = temp;
-    (*head)->next = *head;
+    *head = new;
     return;
+  }
+  if((*head)->next == *head)
+  {
+    new->next = cur;
+    
   }
 
-  if (*head && input < (*head)->valor)
-  {
-    temp->next = *head;
-    *head = temp;
-    return;
-  }
-  if (temp->valor == (*head)->valor)
-  {
-    *head = temp->next;
-    free(temp);
-    return;
-  }
-  Node *current = *head;
-  while (current->next && current->next->valor < input)
-    current = current->next;
-
-  temp->next = current->next;
-  current->next = temp;
-  return;
 }
 
-void show(Node **head)
-{
-  Node *aux = *head;
-  while (aux)
-  {
-    printf("%d ", aux->valor);
-    aux = aux->next;
-  }
-  printf("\n");
-}
 
-void pop(Node **head, int input)
-{
-  if (!head)
-  {
-    printf("Lista vazia\n");
-    return;
-  }
-  Node *temp = *head;
-  while (temp->next && temp->next->valor != input)
-    temp = temp->next;
-
-  if (!temp->next)
-  {
-    printf("Valor nao encontrado\n");
-    return;
-  }
-
-  Node *aux = temp->next;
-  
-  temp->next = (!aux->next) ? aux->next: *head;
-  temp = aux;
-  free(temp);
-  return;
-}
-
-int main()
-{
-  srand(time(NULL));
+int main(){
   Node *head = malloc(sizeof(Node));
-  head = NULL;
-  int input;
-  for (int i = 0; i < 5; i++)
-  {
-    input = rand() % 1000;
-    insertList(&head, input);
-  }
-  input = 207;
-  insertList(&head, input);
-  show(&head);
-
-  pop(&head, 207);
-  show(&head);
-
+  Node *tail = head;
+  head->next = NULL;
+  tail->next = head;
   return 0;
 }
