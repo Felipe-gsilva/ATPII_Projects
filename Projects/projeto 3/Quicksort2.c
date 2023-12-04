ed6761b5b07841385abf0c76f8c103b7f9ed097b
Felipe.gsilva <felipe.gsilva@protonmail.com>
2023-12-04 20:06:14 -0300

    Finished project 3

--- Projects/projeto 3/Quicksort2.c
+++ Projects/projeto 3/Quicksort2.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 
+//função de troca de valores
 void swap(char **a, char **b)
 {
   char *temp = *a;
@@ -10,6 +11,7 @@ void swap(char **a, char **b)
   *b = temp;
 }
 
+// função de impressão do array
 void print_arr(char **arr, int n)
 {
   for (int i = 0; i < n; i++)
@@ -17,35 +19,39 @@ void print_arr(char **arr, int n)
   printf("\n");
 }
 
+// função que retorna o valor do pivô
 int partition(char *arr[], int low, int high, int *pi1)
 {
   if (strcasecmp(arr[low], arr[high]) > 0)
     swap(&arr[low], &arr[high]);
 
+  // seleciona as extremidades como pivôs
   char *pivot1 = arr[low];
   char *pivot2 = arr[high];
 
+  // índices para manter o controle dos elementos menores e maiores
   int i = low + 1;
   int j = high - 1;
   int k = low + 1;
 
+  // loop para colocar os elementos menores que o pivô1 à esquerda e os maiores que o pivô2 à direita
   while (k <= j)
   {
-    if (strcasecmp(arr[k], pivot1) < 0)
+    if (strcasecmp(arr[k], pivot1) < 0) // se o elemento for menor que o pivô1
     {
-      swap(&arr[i], &arr[k]);
+      swap(&arr[i], &arr[k]);           // troca o elemento com o elemento na posição i
       i++;
     }
-    else if (strcasecmp(arr[k], pivot2) > 0)
+    else if (strcasecmp(arr[k], pivot2) > 0) // se o elemento for maior que o pivô2
     {
-      while (strcasecmp(arr[j], pivot2) > 0 && k < j)
+      while (strcasecmp(arr[j], pivot2) > 0 && k < j) // enquanto o elemento for maior que o pivô2 e o índice k for menor que o índice j
         j--;
-      swap(&arr[k], &arr[j]);
+      swap(&arr[k], &arr[j]);                         // troca o elemento com o elemento na posição j
       j--;
-      if (strcasecmp(arr[k], pivot1) < 0)
+      if (strcasecmp(arr[k], pivot1) < 0)             // se o elemento for menor que o pivô1
       {
-        swap(&arr[i], &arr[k]);
-        i++;
+        swap(&arr[i], &arr[k]);                       // troca o elemento com o elemento na posição i
+        i++; 
       }
     }
     k++;
@@ -53,22 +59,22 @@ int partition(char *arr[], int low, int high, int *pi1)
   i--;
   j++;
 
-  swap(&arr[low], &arr[i]);
-  swap(&arr[high], &arr[j]);
+  swap(&arr[low], &arr[i]);  // troca o pivô1 com o elemento na posição i-1
+  swap(&arr[high], &arr[j]); // troca o pivô2 com o elemento na posição j+1
 
   *pi1 = i;
-  return j;
+  return j; // retorna o índice do pivô2
 }
 
 void DualPivotQuickSort(char *arr[], int low, int high)
 {
-  if (low < high)
+  if (low < high) // se o índice low for menor que o índice high
   {
     int pi, pi2;
-    pi2 = partition(arr, low, high, &pi);
-    DualPivotQuickSort(arr, low, pi - 1);
-    DualPivotQuickSort(arr, pi + 1, pi2 - 1);
-    DualPivotQuickSort(arr, pi2 + 1, high);
+    pi2 = partition(arr, low, high, &pi);     // chama a função partition e retorna o índice do pivô2
+    DualPivotQuickSort(arr, low, pi - 1);     // chama a função DualPivotQuickSort para a parte esquerda do pivô1
+    DualPivotQuickSort(arr, pi + 1, pi2 - 1); // chama a função DualPivotQuickSort para a parte entre os pivôs
+    DualPivotQuickSort(arr, pi2 + 1, high);   // chama a função DualPivotQuickSort para a parte direita do pivô2
   }
 }
 
@@ -78,12 +84,12 @@ int main()
   long seconds, nanoseconds;
   double elapsed;
   srand(time(NULL));
-  FILE *stdin;
-  stdin = fopen("in/rep2000u.txt", "r");
+  FILE *stdin, *output;
+  stdin = fopen("arquivo.input", "r");
   int line = 1;
   char c;
 
-  // get how many lines are in the file
+  // conta o número de linhas do arquivo
   do
   {
     c = fgetc(stdin);
@@ -91,22 +97,24 @@ int main()
       line++;
   } while (c != EOF);
 
-  // reset file pointer
+  // verifica se o arquivo está vazio
   if (line == 1)
   {
     puts("Empty file");
     exit(1);
   }
-  printf("Number of lines: %d\n", line);
 
+  // aloca memória para o array
   char **arr;
   arr = malloc(line * sizeof(char *));
   for (int i = 0; i < line; i++)
     arr[i] = malloc(100 * sizeof(char));
 
+  // inicia a repetição para contagem do tempo
   for (int i = 0; i < 5; i++)
   {
-    fseek(stdin, 0, SEEK_SET); // reset file pointer
+    // reseta o ponteiro do arquivo
+    fseek(stdin, 0, SEEK_SET); 
     for (int i = 0; i < line; i++)
       fscanf(stdin, "%s", arr[i]);
 
@@ -122,23 +130,19 @@ int main()
     nanoseconds = end.tv_nsec - begin.tv_nsec;
     elapsed += seconds + nanoseconds * 1e-9;
   }
+  // fecha o arquivo
   fclose(stdin);
-  // anota o tempo de fim em end
   printf("Array after sorting: ");
   print_arr(arr, line);
   printf("Total time measured: %.3f seconds.\n", elapsed);
   printf("Average time per iteration: %.3f seconds.\n", elapsed / 5);
 
-  /*
-  FILE *stdout;
-  stdout = fopen("output.txt", "a");
-  fprintf(stdout, "Dual Pivot Quick Sort:\n");
-  fprintf(stdout, "  -Total time measured: %.3f seconds.\n", elapsed);
-  fprintf(stdout, "  -Average time per iteration: %.3f seconds.\n", elapsed / 5);
-  fprintf(stdout, "\n\n");
-  fclose(stdout);
-  */
-  // free memory
+ // cria o arquivo de saida
+  output = fopen("arquivo.output", "w");
+  for (int i = 0; i < line; i++)
+    fprintf(output, "%s\n", arr[i]);
+  fclose(output);
+  // limpa a memória
   for (int i = 0; i < line; i++)
     free(arr[i]);
   free(arr);
